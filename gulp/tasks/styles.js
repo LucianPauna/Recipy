2992d5670db2016e0638ff1f290bfbbf0cc19ad1
Lucian <paunalucian14@gmail.com>
2019-03-25 13:11:01 +0000

    Completed styling the container div

---
+++ gulp/tasks/styles.js
@@ -0,0 +1,17 @@
+const gulp = require('gulp'),
+postcss = require('gulp-postcss'),
+autoprefixer = require('autoprefixer'),
+cssvars = require('postcss-simple-vars'),
+nested = require('postcss-nested'),
+cssimport = require('postcss-import'),
+mixins = require('postcss-mixins');
+
+gulp.task('styles', () => {
+    return gulp.src('./app/assets/styles/styles.css')
+        .pipe(postcss([cssimport, mixins, cssvars, nested, autoprefixer]))
+        .on('error', (er) => {
+            console.log(er.toString());
+            this.emit('end');
+        })
+        .pipe(gulp.dest('./app/temp/styles'));
+});
